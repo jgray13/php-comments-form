080a3512b67994617309e89017c6bc29c49b2600
Jen Gray <69304519+jgray13@users.noreply.github.com>
2021-06-22 12:56:13 +0100

    Add files via upload

--- form-handler.php
+++ form-handler.php
@@ -5,13 +5,13 @@ $email = '';
 $comments = '';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if (isset($_POST['name'])) {
-        $name = $_POST['name'];
+        $name = trim($_POST['name']);
 	}
 	if (isset($_POST['email'])) {
-        $email = $_POST['email'];
+        $email = trim($_POST['email']);
 	}
 	if (isset($_POST['comments'])) {
-        $comments = $_POST['comments'];
+        $comments = trim($_POST['comments']);
 	}
 
 echo "<p>Thanks for the comment $name...</p>";
